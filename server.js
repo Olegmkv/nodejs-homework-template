b87ec304402d9b660ae91b2063d6dc0c053a65b6
Olegmkv <129410354+Olegmkv@users.noreply.github.com>
2024-01-07 16:52:27 +0200

    server

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+import app from "./app.js"
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);  
+  })
+
+
+
+// const app = require('./app')
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
